2034cb8e3a640d73b1444d545606fe561fa3f75b
Brent Lippert <brentcl@uga.edu>
2016-09-09 15:46:16 -0400

    Fixed error with Map declaration

--- src/main/scala/MLAlgorithms.scala
+++ src/main/scala/MLAlgorithms.scala
@@ -1,3 +1,4 @@
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.tree.model.RandomForestModel
@@ -10,8 +11,12 @@ import org.apache.spark.mllib.util.MLUtils
   */
 object MLAlgorithms {
 
+  val conf = ConfigFactory.load()
+  val sparkConf = new SparkConf().setAppName(conf.getString("spark.appName"))
+  val sc = new SparkContext(sparkConf)
+
   val numClasses = 9
-  val categoricalFeatureInfo = Map()[Int, Int]//Can be used to make certain features (e.g .dll) categorical, for now not used
+  val categoricalFeatureInfo = Map[Int, Int]()//Can be used to make certain features (e.g .dll) categorical, for now not used
   val numTrees = 10
   val featureSubsetStrategy = "auto" //Will use sqrt strategy for numTrees > 1
   val infoGainStrategy = "gini" //Other option entropy, gini better for continuous, entropy better for categorical. (though very little difference, and gini is faster)
@@ -20,6 +25,11 @@ object MLAlgorithms {
   val seed = scala.util.Random.nextLong()
 
 
+  def main(args:Array[String]) = {
+
+    val data = MLUtils.loadLibSVMFile(sc, "blk-fish/small_train_MLUtil.txt")
+
+  }
 
 
 
