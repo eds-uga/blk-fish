5499db10c1cf602ab248bd58462323e5ae3bee7b
brad <bwbazemore@uga.edu>
2016-09-11 19:08:13 -0400

    this is stupid slow trying something else

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -5,7 +5,7 @@
 import org.apache.hadoop.mapred.InvalidInputException
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming._
+import scala.collection.mutable.ListBuffer
 
 object Driver {
   def main(args: Array[String]) = {
@@ -13,6 +13,8 @@ object Driver {
     val sc = new SparkContext(sparkConf)
 
     val filesCats = sc.textFile("gs://project2-csci8360/data/trainLabels.csv").map(line => (line.split(",")(0).replace("\"", ""), line.split(",")(1)))
+    //val trainData = sc.wholeTextFiles("gs://project2-csci8360/data/train/*.bytes")
+    val bytes:ListBuffer[scala.collection.Map[String,Int]]=ListBuffer()
 
     filesCats.toLocalIterator.foreach({
       kv =>
@@ -21,11 +23,14 @@ object Driver {
           val words = temp.flatMap(word => word.split(" "))
           val nomem = words.filter(nums => nums.length <= 2)
           val bytecount = nomem.map(byte => (byte, 1)).reduceByKey(_ + _)
-          bytecount.collect().foreach(print)
+          bytes.append(bytecount.collectAsMap())
         }catch{
           case e: InvalidInputException=>println("ERROR: Missing file")
         }
     })
 
+    bytes.foreach(m=>m.foreach(x => print ("|"+x._1 + "-->" + x._2 +"|")))
+
   }
 }
+
