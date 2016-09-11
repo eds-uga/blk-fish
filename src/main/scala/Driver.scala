27feebc46171a4e34dfdf0230a36f376ed161bf6
brad <bwbazemore@uga.edu>
2016-09-11 19:53:14 -0400

    loading each doc as a in mem string is far faster

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -12,11 +12,11 @@ object Driver {
     val sparkConf = new SparkConf().setAppName("test-preprocess")
     val sc = new SparkContext(sparkConf)
 
-    val filesCats = sc.textFile("gs://project2-csci8360/data/trainLabels.csv").map(line => (line.split(",")(0).replace("\"", ""), line.split(",")(1)))
-    //val trainData = sc.wholeTextFiles("gs://project2-csci8360/data/train/*.bytes")
-    val bytes:ListBuffer[scala.collection.Map[String,Int]]=ListBuffer()
+    val filesCats: RDD[(String, String)] = sc.textFile("gs://project2-csci8360/data/trainLabels.csv").map(line => (line.split(",")(0).replace("\"", ""), line.split(",")(1)))
+    val trainData: RDD[(String, String)] = sc.wholeTextFiles("gs://project2-csci8360/data/train/*.bytes")
+    //val bytes:ListBuffer[scala.collection.Map[String,Int]]=ListBuffer()
 
-    filesCats.toLocalIterator.foreach({
+/*    filesCats.toLocalIterator.foreach({
       kv =>
         try {
           val temp = sc.textFile("gs://project2-csci8360/data/train/" + kv._1 + ".bytes")
@@ -27,9 +27,23 @@ object Driver {
         }catch{
           case e: InvalidInputException=>println("ERROR: Missing file")
         }
+    })*/
+
+    val bytes: RDD[Array[String]] = trainData.map({
+      kv=>
+        kv._2.split(" ")
+        .filter(num=>num.length<=2)
     })
 
-    bytes.foreach(m=>m.foreach(x => print ("|"+x._1 + "-->" + x._2 +"|")))
+    val byteCounts: RDD[Map[String, Int]] = bytes.map({
+      doc=>
+        doc.foldLeft(Map.empty[String, Int]){
+            (count, word) =>
+              count + (word -> (count.getOrElse(word, 0) + 1))
+          }
+    })
+
+    byteCounts.toLocalIterator.foreach(m=>for((k,v) <- m) printf("key: %s, value: %s\n", k, v))
 
   }
 }
