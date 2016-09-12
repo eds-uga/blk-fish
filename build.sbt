ff75c109aea4254fe4c85a1ce3d2f94b2c866871
brad <bwbazemore@uga.edu>
2016-09-11 22:33:49 -0400

    well its working so we are doing ml now

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.0.0",
   "org.apache.spark" % "spark-streaming_2.11" % "2.0.0",
+  "org.apache.spark" %% "spark-mllib" % "2.0.0",
   "com.typesafe" % "config" % "1.3.0"
 )
 
--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -2,20 +2,36 @@
   * Created by bradford_bazemore on 9/4/16.
   */
 
-import org.apache.hadoop.mapred.InvalidInputException
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
-import scala.collection.mutable.ListBuffer
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.RandomForest
+
 
 object Driver {
   def main(args: Array[String]) = {
+
+
+    val numClasses = 9
+    val categoricalFeatureInfo = Map[Int, Int]()//Can be used to make certain features (e.g .dll) categorical, for now not used
+    val numTrees = 10
+    val featureSubsetStrategy = "auto" //Will use sqrt strategy for numTrees > 1
+    val costFunction = "entropy" //Other option entropy, gini better for continuous, entropy better for categorical. (though very little difference, and gini is faster)
+    val maxDepth = 5
+    val maxBins = 32
+
+
+
     val sparkConf = new SparkConf().setAppName("test-preprocess")
     val sc = new SparkContext(sparkConf)
 
     val filesCats: RDD[(String, String)] = sc.textFile("gs://project2-csci8360/data/trainLabels.csv").map(line => (line.split(",")(0).replace("\"", ""), line.split(",")(1)))
-    val trainData: RDD[(String, String)] = sc.wholeTextFiles("gs://project2-csci8360/data/train/*.bytes")
 
-    val bytes: RDD[(String, Array[String])] = trainData.map({
+    val trainDataBytes: RDD[(String, String)] = sc.wholeTextFiles("gs://project2-csci8360/data/train/*.bytes")
+    //val trainDataAsm = sc.wholeTextFiles("gs://project2-csci8360/data/train/*.asm")
+
+    val bytes: RDD[(String, Array[String])] = trainDataBytes.map({
       kv =>
         (
           kv._1,
@@ -24,18 +40,72 @@ object Driver {
           )
     })
 
-    val byteCounts = bytes.map({
+    val bytesS: RDD[(String, Map[String, Double])] = bytes.map({
       doc =>
         (
           doc._1,
-          doc._2.foldLeft(Map.empty[String, Int]) {
-            (count, word) =>
-              count + (word -> (count.getOrElse(word, 0) + 1))
+          doc._2.foldLeft(Map.empty[String, Double]) {
+            (acc: Map[String, Double], word: String) =>
+              acc + (word -> (acc.getOrElse(word, 0.0) + 1.0))
           }
           )
     })
 
-    println(byteCounts.count())
+    val byteCounts: RDD[(String, Map[Int, Double])] = bytesS.map({
+      doc =>
+        (
+          doc._1,
+          doc._2.map(
+            byte =>
+              (Integer.parseInt(byte._1,16),byte._2)
+            )
+          )
+    })
+
+    val trainPoints: RDD[LabeledPoint] = byteCounts.map({
+      point=>
+        LabeledPoint(
+          filesCats.lookup(point._1).head.toDouble,
+          Vectors.sparse(
+            point._2.size,
+            point._2.keySet.toArray,
+            point._2.values.toArray
+          )
+        )
+    })
+
+
+
+    val splits = trainPoints.randomSplit(Array(.75,.25))
+    val (trainingData, testingData) = (splits(0), splits(1))
+
+    //training
+    val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeatureInfo, numTrees, featureSubsetStrategy, costFunction, maxDepth, maxBins)
+
+    //testing
+    val labelAndPreds = testingData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+
+    //output results
+    labelAndPreds.foreach(println)
+    val correct = labelAndPreds.filter(x => x._1 == x._2)
+    println("correct: " + correct.count)
+    val incorrect = labelAndPreds.filter(x => x._1 != x._2)
+    println("incorrect: " + incorrect.count)
+    val percentage = correct.count.toDouble/(correct.count.toDouble + incorrect.count.toDouble)
+    println("PERCENTAGE: " + percentage)
+
+/*    val dlls: RDD[(String, Array[String])] = trainDataAsm.map({
+      kv =>
+      (
+        kv._1,
+        kv._2.split(" ")
+          .filter(word=>word.contains(".dll")|word.contains(".DLL"))
+          .filter(word=>word.matches("\\w+\\.(dll)|(DLL)"))
+        )
+    })*/
 
   }
 }
